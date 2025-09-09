7ddf1a38f9c5ebb2a069d06f4830c9941691a29e
ssandhiya8335-oss <ssandhiya8335@gmail.com>
2025-09-09 20:57:23 +0530

    Create Index.js

---
+++ Index.js
@@ -0,0 +1,36 @@
+cols="40" rows="3"
+                              title="Enter Your Achievements">
+                    </textarea>
+                    <textarea name="contact" 
+                              placeholder="Contact Information" 
+                              cols="40" rows="3"
+                              title="Enter Your Contact Information">
+                    </textarea>
+                </form>
+            </div>
+            <p class="credit">
+                Created by
+                <a href=
+"https://www.geeksforgeeks.org/user/lunatic1/contributions/" 
+                   target="_blank">
+                      lunatic1
+                </a>
+            </p>
+            <p class="tip">
+                **You can use markup in the text area for text decoration**
+            </p>
+        </div>
+    </section>
+    <!-- input container ends here -->
+    <!-- output container starts here -->
+    <div class="output_container"></div>
+    <!-- output container ends here -->
+
+    <!-- preview button -->
+    <button onclick="hide()">
+          Generate / Edit
+      </button>
+    <!-- script link here -->
+    <script src="script.js"></script>
+</body>
+</html>
